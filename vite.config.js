0530e281dd18bd0b7c893ce9676d754c30d5935f
gN4HT <tranthang75142@gmail.com>
2025-04-07 19:50:50 +0700

    build

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit()],
-  build: {
-    rollupOptions: {
-      external: ["drizzle-orm", "@oslojs/crypto/sha2", "@oslojs/encoding"],
-    },
-  },
 });
